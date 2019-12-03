ff8b54f308c22cd61f23945f9fee0fc7cf12f13c
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-12-03 18:21:54 +0900

    error hendler 추가

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 import v1Route from './routes/v1'
 
-var app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use('/v1', v1Route)
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
 // error handler
 app.use((err, req, res, next) => {
@@ -41,4 +37,5 @@ app.use((err, req, res, next) => {
     .json({ message: apiError.message })
 })
 
-module.exports = app;
+// bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
+module.exports = app
