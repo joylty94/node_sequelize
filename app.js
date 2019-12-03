45a019682fc7e35f9b4f630023ef088eeef8ca68
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-12-03 18:20:40 +0900

    통신 확인

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import v1Route from './routes/v1'
 
 var app = express();
 
@@ -20,9 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/v1', v1Route)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ controllers/v1/user.controller.js
@@ -0,0 +1,12 @@
+
+const get = async (req, res, next) => {
+    try {
+        return res.json({ message: 'users get' })
+    } catch (e) {
+        next(e)
+    }
+}
+
+export {
+    get
+}
