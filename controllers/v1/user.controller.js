40677d3f988314788bf9b9aacc47efa321e407cf
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-12-04 11:27:07 +0900

    join api 추가

--- controllers/v1/user.controller.js
+++ controllers/v1/user.controller.js
@@ -3,7 +3,14 @@ import { models } from '../../models'
 
 const get = async (req, res, next) => {
     try {
-        const users = await models.User.findAll()
+        const users = await models.User.create(
+            {
+                email: 'test56@test.com',
+                password: 'test123456',
+                createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
+                updatedAt: moment().format('YYYY-MM-DD hh:mm:ss')
+            }
+        )
 
         return res.json(users)
     } catch (e) {
